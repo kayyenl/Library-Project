b7cacf05c3a8145cd66d4991a6ceb00897620c6e
kayyenl <yenyenlee9@gmail.com>
2022-07-31 17:08:25 +0800

    going to take a small break

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Book from '../components/ui/Book';
+import { books } from '../data'
 
 const Books = () => {
     return (
@@ -9,7 +11,18 @@ const Books = () => {
                         <div className="row">
                             <div className="books__header">
                                 <h2 className='section__title books__header--title'>All Books</h2>
-                                <select id="filter"></select>
+                                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event)}>
+                                    <option value="DEFAULT" disabled>Sort by...</option>
+                                    <option value="LOW_TO_HIGH">Price, Low to High</option>
+                                    <option value="HIGH_TO_LOW">Price, High to Low</option>
+                                    <option value="RATING">Rating</option>
+                                </select>
+                            </div>
+                            <div className="books">
+                                {
+                                    books.map(elem => 
+                                        <Book key = {elem.id} info = {elem}/>)
+                                }
                             </div>
                         </div>
                     </div>
