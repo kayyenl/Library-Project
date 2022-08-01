aa2ecbb30dd0e3a44c82711a19f028097d02e2f1
kayyenl <yenyenlee9@gmail.com>
2022-08-01 15:13:00 +0800

    implemented the ratings for the website /book

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -4,7 +4,15 @@ import Book from '../components/ui/Book';
 const Books = ( {books : initialBooks} ) => {
     const [books, setBooks] = useState(initialBooks)
     function filterBooks(filter) {
-        books.sort((a,b) => a - b)
+        if (filter === "LOW_TO_HIGH") {
+            setBooks(books.slice().sort((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
+        }
+        if (filter === "HIGH_TO_LOW") {
+            setBooks(books.slice().sort((a,b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)))
+        }
+        if (filter === "RATING") {
+            setBooks(books.slice().sort((a,b) => (b.rating || b.rating) - (a.rating || a.rating)))
+        }
     }
     return (
         <div id='books__body'>
